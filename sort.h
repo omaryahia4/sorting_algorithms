f2b0269cd32ee1b20d09f33ec3c8a050e3edd3dc
omaryahia4 <omar.yahia4444@gmail.com>
2021-06-15 11:25:48 +0000

    header file

--- sort.h
+++ sort.h
@@ -31,5 +31,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 #endif
