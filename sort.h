a034be0a3e7f10071d030b645375c2b4e3611769
omaryahia4 <omar.yahia4444@gmail.com>
2021-06-15 11:23:38 +0000

    header file

--- sort.h
+++ sort.h
@@ -3,40 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio.h>
-typedef enum kind_e
-{
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
-} kind_t;
 
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-    const char *value;
-    const kind_t kind;
-} card_t;
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
-} deck_node_t;
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -46,9 +13,9 @@ typedef struct deck_node_s
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
